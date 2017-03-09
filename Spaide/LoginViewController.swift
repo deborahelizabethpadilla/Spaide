8a5de2d8c9a13c299a35d05696de335d446b497e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-09 13:21:44 -0500

    Login VC: Add File To Storyboard

---
+++ Spaide/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Spaide
+//
+//  Created by Deborah on 3/9/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //Outlets And Actions
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
