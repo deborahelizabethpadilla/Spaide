e22d5f34958f1ebf8eb93f2e79828e47f85fc93e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-13 15:07:23 -0400

    Messages File

--- Spaide/Messages.swift
+++ Spaide/Messages.swift
@@ -16,47 +16,54 @@ protocol MessageReceivedDelegate: class {
 }
 
 class MessagesHandler {
-    private static let _instance = MessagesHandler();
+    
+    //Variables
+    
+    private static let _instance = MessagesHandler()
     private init() {}
     
-    weak var delegate: MessageReceivedDelegate?;
+    weak var delegate: MessageReceivedDelegate?
     
     static var Instance: MessagesHandler {
-        return _instance;
+        
+        return _instance
     }
     
     func sendMessage(senderID: String, senderName: String, text: String) {
         
-        let data: Dictionary<String, Any> = [Constants.SENDER_ID: senderID, Constants.SENDER_NAME: senderName, Constants.TEXT: text];
+        let data: Dictionary<String, Any> = [Constants.SENDER_ID: senderID, Constants.SENDER_NAME: senderName, Constants.TEXT: text]
         
-        DBProvider.Instance.messagesRef.childByAutoId().setValue(data);
+        DataBase.Instance.messagesRef.childByAutoId().setValue(data)
         
     }
     
     func sendMediaMessage(senderID: String, senderName: String, url: String) {
-        let data: Dictionary<String, Any> = [Constants.SENDER_ID: senderID, Constants.SENDER_NAME: senderName, Constants.URL: url];
         
-        DBProvider.Instance.mediaMessagesRef.childByAutoId().setValue(data);
+        let data: Dictionary<String, Any> = [Constants.SENDER_ID: senderID, Constants.SENDER_NAME: senderName, Constants.URL: url]
+        
+        DataBase.Instance.mediaMessagesRef.childByAutoId().setValue(data)
     }
     
     func sendMedia(image: Data?, video: URL?, senderID: String, senderName: String) {
         
         if image != nil {
             
-            DBProvider.Instance.imageStorageRef.child(senderID + "\(NSUUID().uuidString).jpg").put(image!, metadata: nil) { (metadata: FIRStorageMetadata?, err: Error?) in
+            DataBase.Instance.imageStorageRef.child(senderID + "\(NSUUID().uuidString).jpg").put(image!, metadata: nil) { (metadata: FIRStorageMetadata?, err: Error?) in
                 
                 if err != nil {
                     
                     //Let User Know Uploading Image Has Failed
                     
                 } else {
+                    
                     self.sendMediaMessage(senderID: senderID, senderName: senderName, url: String(describing: metadata!.downloadURL()!));
                 }
                 
             }
             
         } else {
-            DBProvider.Instance.videoStorageRef.child(senderID + "\(NSUUID().uuidString)").putFile(video!, metadata: nil) { (metadata: FIRStorageMetadata?, err: Error?) in
+            
+            DataBase.Instance.videoStorageRef.child(senderID + "\(NSUUID().uuidString)").putFile(video!, metadata: nil) { (metadata: FIRStorageMetadata?, err: Error?) in
                 
                 if err != nil {
                     
@@ -73,13 +80,18 @@ class MessagesHandler {
     }
     
     func observeMessages() {
-        DBProvider.Instance.messagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
+        
+        DataBase.Instance.messagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
             
             if let data = snapshot.value as? NSDictionary {
+                
                 if let senderID = data[Constants.SENDER_ID] as? String {
+                    
                     if let senderName = data[Constants.SENDER_NAME] as? String {
+                        
                         if let text = data[Constants.TEXT] as? String {
-                            self.delegate?.messageReceived(senderID: senderID, senderName: senderName, text: text);
+                            
+                            self.delegate?.messageReceived(senderID: senderID, senderName: senderName, text: text)
                         }
                     }
                 }
@@ -89,13 +101,18 @@ class MessagesHandler {
     }
     
     func observeMediaMessages() {
-        DBProvider.Instance.mediaMessagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
+        
+        DataBase.Instance.mediaMessagesRef.observe(FIRDataEventType.childAdded) { (snapshot: FIRDataSnapshot) in
             
             if let data = snapshot.value as? NSDictionary {
+                
                 if let id = data[Constants.SENDER_ID] as? String {
+                    
                     if let name = data[Constants.SENDER_NAME] as? String {
+                        
                         if let fileURL = data[Constants.URL] as? String {
-                            self.delegate?.mediaReceived(senderID: id, senderName: name, url: fileURL);
+                            
+                            self.delegate?.mediaReceived(senderID: id, senderName: name, url: fileURL)
                         }
                     }
                 }
