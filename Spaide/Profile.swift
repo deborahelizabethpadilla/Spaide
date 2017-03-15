6538c85340c5e2c0917f5eb591e5185b17d22d97
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-15 14:07:46 -0400

    Edit: Image View For Profile. Make Image View A Circle

--- Spaide/Profile.swift
+++ Spaide/Profile.swift
@@ -11,6 +11,15 @@ import AVKit
 
 class Profile: UIViewController {
     
+    @IBOutlet var profileView: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Make Profile View A Circle
+        
+        profileView.layer.cornerRadius = profileView.frame.size.width / 2
+        profileView.clipsToBounds = true
+    }
 }
 
