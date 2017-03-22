5943011bd4099d6ae5575139dfbc2024c1548725
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-22 13:58:51 -0400

    Edits: Storyboard
    Added background from Chameleon framework
    to input view controller.

---
+++ Spaide/InputController.swift
@@ -0,0 +1,22 @@
+//
+//  InputController.swift
+//  Spaide
+//
+//  Created by Deborah on 3/22/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class InputController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set Background
+        
+        view.backgroundColor = FlatGreenDark()
+    }
+}
