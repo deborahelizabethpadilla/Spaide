b758638d3779947f2f0cdbb4f2b8a0203561106e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-22 15:56:02 -0400

    Edits: Refactor
    Refactored code and made big changes to some
    swift view controller files. Redoing layout.

--- Spaide/InputController.swift
+++ Spaide/InputController.swift
@@ -8,16 +8,56 @@
 
 import UIKit
 import ChameleonFramework
+import Firebase
 
 class InputController: UIViewController {
     
     //Outlets
     
     @IBOutlet var postLabel: UIButton!
+    @IBOutlet var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Variables
+        
+        _ = FIRDatabase.database().reference()
+        let storage = FIRStorage.storage().reference()
+        let tempImageRef = storage.child("tmpDir/tmpImage.jpg")
+        
+        /*
+        
+        let image = UIImage(named: "firebaseImage.jpg")
+        let metaData = FIRStorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        tempImageRef.put(UIImageJPEGRepresentation(image!, 0.0)!, metadata: metaData) { (data, error) in
+            if error == nil {
+                
+                print("Upload Success!")
+            } else {
+                
+                print(error?.localizedDescription)
+            }
+        }
+        
+        */
+        
+        tempImageRef.data(withMaxSize: Int64(1*1000*1000)) { (data, error) in
+            
+            if error == nil {
+                
+                print(data!)
+                
+                self.imageView.image = UIImage(data: data!)
+                
+            } else {
+                
+                print(error?.localizedDescription as Any)
+            }
+        }
         
         //Set Background
         
@@ -29,7 +69,6 @@ class InputController: UIViewController {
         postLabel.layer.cornerRadius = 5
         postLabel.layer.borderWidth = 1
         postLabel.layer.borderColor = UIColor.flatWatermelonDark.cgColor
-        
-        
+    
     }
 }
--- Spaide/PostController.swift
+++ Spaide/PostController.swift
@@ -11,107 +11,20 @@ import ChameleonFramework
 import Firebase
 import FirebaseDatabase
 
-//Exactly What Post Will Read
-
-struct postStruct {
-    
-    let title: String!
-    let message: String!
-}
-
 class PostController: UITableViewController {
     
     //Let Posts Be In Array
     
-    let posts = [postStruct]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Grab From Database
-        
-        let databaseRef = FIRDatabase.database().reference()
-        
-        databaseRef.child("Posts").queryOrderedByKey().observe(.childAdded, with:  {
-            snapshot in
-            
-            let title = snapshot.value!["title"] as! String
-            let message = snapshot.value!["message"] as! String
-            let uLocation = snapshot.value!["uLocation"] as! String
-            let needs = snapshot.value!["needs"] as! String
-            let uImage = snapshot.value!["uImage"] as! UIImage
-            
-            posts.insert(postStruct(title: title, message: message, uLocation: uLocation, needs: needs, uImage: uImage) ,at: 0)
-            self.tableView.reloadData()
-            
-    })
-        
-        //Post
-        
-        post()
-        
+    
         //Set Background Color
         
         view.backgroundColor = FlatGreenDark()
         
     }
     
-    func post() {
-        
-        //Store In Database
-        
-        let title = "Title"
-        let message = "Message"
-        let uLocation = "Location"
-        let needs = "Needs"
-        let uImage = UIImage()
-        
-        //Key For What Were Uploading
-        
-        let post : [String : AnyObject] = ["title" : title as AnyObject, "message" : message as AnyObject, "uLocation" : uLocation as AnyObject, "needs" : needs as AnyObject, "uImage" :uImage]
-        
-        //Store In Database
-        
-        let databaseRef = FIRDatabase.database().reference()
-        
-        //Reference Part Of Database
-        
-        databaseRef.child("Posts").childByAutoId().setValue(post)
-    
-    }
-    
-    //Table View Functions
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        //Count How Many Things Inside Array
-        
-        return posts.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        //Post In Cell
-        
-        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        
-        let label1 = cell?.viewWithTag(1) as! UILabel
-        label1.text = posts[indexPath.row].title
-        
-        let label2 = cell?.viewWithTag(2) as! UILabel
-        label2.text = posts[indexPath.row].message
-        
-        let label3 = cell?.viewWithTag(2) as! UILabel
-        label3.text = posts[indexPath.row].uLocation
-        
-        let label4 = cell?.viewWithTag(2) as! UILabel
-        label4.text = posts[indexPath.row].needs
-        
-        var UIImage = cell?.viewWithTag(2) as! UIImage
-        UIImage = posts[indexPath.row].uImage
-        
-        
-        return cell!
-    }
 
 }
