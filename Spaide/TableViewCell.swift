bc548d582906528dc66060d343759f7276fba3e7
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-28 00:10:37 -0400

    Edit: Table View Cell
    Added info so I can use the table view
    cell. Still need to add more code.

---
+++ Spaide/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  Spaide
+//
+//  Created by Deborah on 3/28/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    
+}
