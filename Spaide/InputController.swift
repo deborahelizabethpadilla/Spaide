f6b7f180ce047844dba287d6237881193380a73b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-27 23:58:40 -0400

    Add: Core Data
    Added Core Data entities, stack and other information
    which helps save the photo, as well as the cell. Still
    need to add more code.

--- Spaide/InputController.swift
+++ Spaide/InputController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import ChameleonFramework
-import ImageRow
-import Eureka
 
 class InputController: UIViewController, FormViewController {
     
@@ -22,32 +20,7 @@ class InputController: UIViewController, FormViewController {
         
       view.backgroundColor = FlatGreenDark()
         
-      //Row With Eureka
-        
-        form +++ Section()
-            <<< ImageRow() { row in
-                row.title = "Image Row 1"
-                row.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
-                row.clearAction = .yes(style: UIAlertActionStyle.destructive)
-            }
-            +++
-            Section()
-            <<< ImageRow() {
-                $0.title = "Image Row 2"
-                $0.sourceTypes = .PhotoLibrary
-                $0.clearAction = .no
-                }
-                .cellUpdate { cell, row in
-                    cell.accessoryView?.layer.cornerRadius = 17
-                    cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
-            }
-            +++
-            Section()
-            <<< ImageRow() {
-                $0.title = "Image Row 3"
-                $0.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
-                $0.clearAction = .yes(style: .default)
-        }
+    
     }
     
     
