58caa4d736856be2b35c3c3f98554f957a991ceb
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-28 00:15:29 -0400

    Add: Post View Controller
    Added code to post view controller so users
    can see information being provided.

--- Spaide/PostController.swift
+++ Spaide/PostController.swift
@@ -13,10 +13,64 @@ import FirebaseDatabase
 
 class PostController: UITableViewController {
     
-    //Let Posts Be In Array
+    //Outlets
     
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
+    
+    //Get Photos
+    
+    func initWithPhoto(_ photo: Photo) {
+        
+        if photo.imageData != nil {
+            
+            DispatchQueue.main.async {
+                
+                self.imageView.image = UIImage(data: photo.imageData! as Data)
+                self.activityIndicator.stopAnimating()
+            }
+            
+        } else {
+            
+            downloadImage(photo)
+        }
+    }
+    
+    //Download Images
+    
+    func downloadImage(_ photo: Photo) {
+        
+        URLSession.shared.dataTask(with: URL(string: photo.imageURL!)!) { (data, response, error) in
+            if error == nil {
+                
+                DispatchQueue.main.async {
+                    
+                    self.imageView.image = UIImage(data: data! as Data)
+                    self.activityIndicator.stopAnimating()
+                    self.saveImageDataToCoreData(photo: photo, imageData: data! as NSData)
+                }
+            }
+            
+            }
+            
+            .resume()
+    }
+    
+    //Save Images
+    
+    func saveImageDataToCoreData(photo: Photo, imageData: NSData) {
+        do {
+            photo.imageData = imageData
+            let delegate = UIApplication.shared.delegate as! AppDelegate
+            let stack = delegate.stack
+            try stack.saveContext()
+        } catch {
+            print("Saving Photo imageData Failed")
+        }
+    }
+    
+}
     override func viewDidLoad() {
         super.viewDidLoad()
     
