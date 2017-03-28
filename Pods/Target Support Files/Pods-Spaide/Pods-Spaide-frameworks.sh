cf8cfcb66688aeec2311daeade68e7cfbe4ab05e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-27 23:22:57 -0400

    Edits: Removed Pod

--- Pods/Target Support Files/Pods-Spaide/Pods-Spaide-frameworks.sh
+++ Pods/Target Support Files/Pods-Spaide/Pods-Spaide-frameworks.sh
@@ -90,14 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ChameleonFramework/ChameleonFramework.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TextFieldEffects/TextFieldEffects.framework"
--- Spaide/InputController.swift
+++ Spaide/InputController.swift
@@ -8,14 +8,12 @@
 
 import UIKit
 import ChameleonFramework
-import Firebase
+import ImageRow
+import Eureka
 
-class InputController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class InputController: UIViewController, FormViewController {
     
     //Outlets & Actions
-    
-
- 
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +22,33 @@ class InputController: UIViewController, UIImagePickerControllerDelegate, UINavi
         
       view.backgroundColor = FlatGreenDark()
         
-      //Storage
+      //Row With Eureka
         
-        
-    
+        form +++ Section()
+            <<< ImageRow() { row in
+                row.title = "Image Row 1"
+                row.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
+                row.clearAction = .yes(style: UIAlertActionStyle.destructive)
+            }
+            +++
+            Section()
+            <<< ImageRow() {
+                $0.title = "Image Row 2"
+                $0.sourceTypes = .PhotoLibrary
+                $0.clearAction = .no
+                }
+                .cellUpdate { cell, row in
+                    cell.accessoryView?.layer.cornerRadius = 17
+                    cell.accessoryView?.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+            }
+            +++
+            Section()
+            <<< ImageRow() {
+                $0.title = "Image Row 3"
+                $0.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
+                $0.clearAction = .yes(style: .default)
+        }
     }
+    
     
 }
