6123057d172d8dd75f3138c3c856dbd57ea7b777
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-04-10 10:29:57 -0400

    Edit: Remove Core Data
    Stick with Firebase.

--- Spaide/AppDelegate.swift
+++ Spaide/AppDelegate.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 import Firebase
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let stack = CoreDataStack(modelName: "CoreDataModel")!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
