3d4e8195cb4609f2778978967124636bf8de1ba4
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-05-01 17:34:02 -0400

    Change Cell

--- Spaide/UserTableViewController.swift
+++ Spaide/UserTableViewController.swift
@@ -12,6 +12,10 @@ import FirebaseDatabase
 
 class UserTableViewController: UITableViewController {
     
+    //Outlets
+    
+    
+    
     //Variables
     
     var ref: FIRDatabaseReference!
@@ -40,6 +44,8 @@ class UserTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        //Inside The Cell
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "userCell")
         
         cell?.textLabel?.text = userInfo[indexPath.row].firstName
@@ -47,13 +53,17 @@ class UserTableViewController: UITableViewController {
         cell?.textLabel?.text = userInfo[indexPath.row].limits
         cell?.imageView?.image = userInfo[indexPath.row].profilePhoto
         
+        //Boder Cell Color
+        
+        cell?.layer.borderWidth = 4.0
+        cell?.layer.borderColor = UIColor.flatGreen.cgColor
         
         return cell!
     }
     
     func fetchUsersInfo() {
         
-        refHandle = ref.child("User Profile").observe(.childAdded, with: { (snapshot) in
+        refHandle = ref.child("User Posts").observe(.childAdded, with: { (snapshot) in
             
             if let dictionary = snapshot.value as? [String : AnyObject] {
                 
