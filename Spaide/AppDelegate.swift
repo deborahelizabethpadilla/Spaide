7a2149246bf17626b3cba47aeff4fda3793c4804
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-05-15 13:45:50 -0400

    Facebook API Update

--- Spaide/AppDelegate.swift
+++ Spaide/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //Configure Facebook API
         
-        FBSDKLoginButton.classForCoder()
+        FBSDKButton.classForCoder()
         
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
