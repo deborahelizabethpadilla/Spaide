a824630ef857afdebc08a4015ad8216acb35f067
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-05-19 19:04:34 -0400

    changes

---
+++ Spaide/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  Spaide
+//
+//  Created by Deborah on 5/19/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Tab Bar Color
+        
+        self.tabBar.barTintColor = UIColor.white
+    }
+
+}
