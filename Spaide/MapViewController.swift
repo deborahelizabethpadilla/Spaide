3115205c12ceb99a6032feed2fe2bd6256476cf4
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-28 12:11:21 -0400

    Changes

---
+++ Spaide/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MapViewController.swift
+//  Spaide
+//
+//  Created by Deborah on 8/28/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBAction func showSearchBar(_ sender: Any) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+} //End Class
