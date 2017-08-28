a2181ebc4982a6d664e40eccec22d946fccb5933
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-28 12:59:31 -0400

    Changes

--- Spaide/MapViewController.swift
+++ Spaide/MapViewController.swift
@@ -19,19 +19,7 @@ class MapViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var mapView: MKMapView!
     @IBAction func showSearchBar(_ sender: Any) {
         
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.hidesNavigationBarDuringPresentation = false
-        self.searchController.searchBar.delegate = self
-        present(searchController, animated: true, completion: nil)
         
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-        
-        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTableViewController") as! LocationSearchTableViewController
-        searchController = UISearchController(searchResultsController: locationSearchTable)
-        searchController?.searchResultsUpdater = locationSearchTable
     }
     
     override func viewDidLoad() {
@@ -42,22 +30,3 @@ class MapViewController: UIViewController, UISearchBarDelegate {
 
 } //End Class
 
-extension MapViewController : CLLocationManagerDelegate {
-    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
-            locationManager.requestLocation()
-        }
-    }
-    
-    private func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            let span = MKCoordinateSpanMake(0.05, 0.05)
-            let region = MKCoordinateRegion(center: location.coordinate, span: span)
-            mapView.setRegion(region, animated: true)
-        }
-    }
-    
-    private func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print("error:: \(error)")
-    }
-}
