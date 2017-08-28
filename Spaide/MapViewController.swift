f737920754ac09082c3dd477d7e511a822838c8b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-28 12:14:17 -0400

    Search Bar

--- Spaide/MapViewController.swift
+++ Spaide/MapViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UISearchBarDelegate {
+    
+    //Variables
+    
+    var searchController:UISearchController!
+    var annotation:MKAnnotation!
+    var localSearchRequest:MKLocalSearchRequest!
+    var localSearch:MKLocalSearch!
+    var localSearchResponse:MKLocalSearchResponse!
+    var error:NSError!
+    var pointAnnotation:MKPointAnnotation!
+    var pinAnnotationView:MKPinAnnotationView!
 
     @IBOutlet weak var mapView: MKMapView!
     @IBAction func showSearchBar(_ sender: Any) {
         
-        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
