c288307e6c42f6958a94c7bcc2e7d8bdafe71f33
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-08-29 14:04:43 -0400

    Edits

--- Spaide/SettingsTableViewController.swift
+++ Spaide/SettingsTableViewController.swift
@@ -7,84 +7,124 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import SVProgressHUD
 
-class SettingsTableViewController: UITableViewController {
-
+class SettingsTableViewController: UITableViewController, UITextFieldDelegate {
+    //Outlets
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    //Actions
+    @IBAction func saveEmail(_ sender: Any) {
+        updateEmail()
+    }
+    
+    @IBAction func savePassword(_ sender: Any) {
+        updatePassword()
+    }
+    
+    @IBAction func contactAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        
+    }
+    
+    @IBAction func deleteAccount(_ sender: Any) {
+        deleteAccount()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        //Close Keyboard With Return Key
+        
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+        
+        //Close Keyboard With Tap
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SettingsTableViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    //Functions
+    
+    func dismissKeyboard() {
+        
+        //Close Keyboard On Tap
+        
+        view.endEditing(true)
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func updateEmail() {
+        
+        SVProgressHUD.show(withStatus: "Updating Email...")
+        
+        if let user = Auth.auth().currentUser {
+            
+            user.updateEmail(to: emailField.text!, completion: { (error) in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                    
+                    SVProgressHUD.dismiss()
+                    SVProgressHUD.showError(withStatus: "Email Update Failed!")
+                    
+                } else {
+                    
+                    SVProgressHUD.dismiss()
+                }
+            })
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    func updatePassword() {
+        
+        
+        SVProgressHUD.show(withStatus: "Updating Password...")
+        
+        if let user = Auth.auth().currentUser {
+            
+            user.updatePassword(to: passwordField.text!, completion: { (error) in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                    
+                    SVProgressHUD.dismiss()
+                    SVProgressHUD.showError(withStatus: "Password Update Failed!")
+                    
+                } else {
+                    
+                    SVProgressHUD.dismiss()
+                }
+            })
+        }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func deleteAccount() {
+        
+        SVProgressHUD.show(withStatus: "Deleting Account...")
+        
+        let user = Auth.auth().currentUser
+        user?.delete(completion: { (error) in
+            if let error = error {
+                
+                print(error.localizedDescription)
+                
+                SVProgressHUD.dismiss()
+                
+                SVProgressHUD.showError(withStatus: "Failed To Delete Account!")
+                
+            } else {
+                
+                SVProgressHUD.dismiss()
+            }
+        })
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 } //End Class
