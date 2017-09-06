c72dc317165879ba3b22697b91c9405377ac1a50
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-09-06 16:00:39 -0400

    Changes

--- Spaide/AppDelegate.swift
+++ Spaide/AppDelegate.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import Firebase
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let stack = CoreDataStack(modelName: "Spaide")!
 
     var window: UIWindow?
 
@@ -48,6 +51,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
- }
+
+}
 
--- Spaide/MapViewController.swift
+++ Spaide/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 import Firebase
 import FirebaseDatabase
 import GeoFire
@@ -18,33 +19,158 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     var ref:DatabaseReference!
     var geoFire:GeoFire!
     let locationManager = CLLocationManager()
+    var currentPins:[Pin] = []
     
     //Outlets & Actions
     @IBOutlet weak var mapView: MKMapView!
     @IBAction func addLocation(_ sender: Any) {
+        //Present VC To Add Info
         
+    }
+    
+    //Core Data
+    
+    func getCoreDataStack() -> CoreDataStack {
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.stack
+    }
+    
+    //Fetch Results
+    
+    func getFetchedResultsController() -> NSFetchedResultsController<NSFetchRequestResult> {
+        
+        let stack = getCoreDataStack()
+        
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
+        fr.sortDescriptors = []
+        
+        return NSFetchedResultsController(fetchRequest: fr, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+    }
+    
+    //Load Saved Pin
+    
+    func preloadSavedPin() -> [Pin]? {
+        
+        do {
+            
+            var pinArray:[Pin] = []
+            let fetchedResultsController = getFetchedResultsController()
+            try fetchedResultsController.performFetch()
+            let pinCount = try fetchedResultsController.managedObjectContext.count(for: fetchedResultsController.fetchRequest)
+            for index in 0..<pinCount {
+                
+                pinArray.append(fetchedResultsController.object(at: IndexPath(row: index, section: 0)) as! Pin)
+            }
+            
+            return pinArray
+            
+        } catch {
+            
+            return nil
+        }
+    }
+    
+    //Add Pin
+    
+    func addAnnotationToMap(fromPoint: CGPoint) {
+        
+        let coordToAdd = mapView.convert(fromPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordToAdd
+        addCoreData(of: annotation)
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+    func addAnnotationToMap(fromCoord: CLLocationCoordinate2D) {
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = fromCoord
+        mapView.addAnnotation(annotation)
+    }
+    
+    //Add Core Data
+    
+    func addCoreData(of: MKAnnotation) {
+        
+        do {
+            
+            let coord = of.coordinate
+            let pin = Pin(latitude: coord.latitude, longitude: coord.longitude, context: getCoreDataStack().context)
+            try getCoreDataStack().saveContext()
+            currentPins.append(pin)
+            
+        } catch {
+            
+            print("Add Core Data Failed")
+        }
+    }
+    
+    //Map View Function
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        if !editMode {
+            
+            performSegue(withIdentifier: "PinPhotos", sender: view.annotation?.coordinate)
+            
+            mapView.deselectAnnotation(view.annotation, animated: false)
+            
+        } else {
+            
+            removeCoreData(of: view.annotation!)
+            
+            mapView.removeAnnotation(view.annotation!)
+        }
+    }
+    
+    //Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "PinPhotos" {
+            
+            let destination = segue.destination as! PhotosViewController
+            let coord = sender as! CLLocationCoordinate2D
+            destination.coordinateSelected = coord
+            
+            for pin in currentPins {
+                
+                if pin.latitude == coord.latitude && pin.longitude == coord.longitude {
+                    
+                    destination.coreDataPin = pin
+                    break
+                }
+            }
+            
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Delegate
-        mapView.delegate = self
         
         //Track Mode
         mapView.userTrackingMode = MKUserTrackingMode.follow
         
-        //Firebase & GeoFire
-        ref = Database.database().reference()
-        geoFire = GeoFire(firebaseRef: ref)
+        //Pins
         
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let savedPins = preloadSavedPin()
         
-    }
-    
-    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
-        
+        if savedPins != nil {
+            
+            currentPins = savedPins!
+            
+            //Add Annotation To Map
+            
+            for pin in currentPins {
+                
+                let coord = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
+                addAnnotationToMap(fromCoord: coord)
+                
+            }
+        }
+
     }
     
     //Display Alert
